e77eab2efcc580c4317fc8834ddf43e5f618d67c
hwangwoojin <hwangwoojin.dev@gmail.com>
2021-11-07 20:06:20 +0900

    :sparkles: 데이터셋 명세를 위한 XML 파서 구현
    AI 허브 데이터셋 명세를 파싱하기 위한 XML 파서 구현

---
+++ xml_parser.py
@@ -0,0 +1,36 @@
+import xml.etree.ElementTree as ET
+
+FILENAME = './videos/2.xml'
+
+doc = ET.parse(FILENAME)
+
+root = doc.getroot()
+
+objects = root.findall('object')
+
+for person in objects:
+    objectname = person.findtext('objectname')
+
+    position = person.find('position')
+    keyframe = position.findtext('keyframe')
+    keypoint_x = position.find('keypoint').findtext('x')
+    keypoint_y = position.find('keypoint').findtext('y')
+
+    actions = person.findall('action')
+    action_list = {}
+    for action in actions:
+        action_name = action.findtext('actionname')
+        action_list[action_name] = []
+        
+        action_frames = action.findall('frame')
+        for action_frame in action_frames:
+            start = action_frame.findtext('start')
+            end = action_frame.findtext('end')
+            action_list[action_name].append([start, end])
+        
+    print(f'objectname: {objectname}')
+    print(f'keyframe: {keyframe}')
+    print(f'keypoint_x: {keypoint_x}')
+    print(f'keypoint_y: {keypoint_y}')
+    print(f'action: \n{action_list}')
+    print()
